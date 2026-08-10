860e489f4791b9ebafc4692d7a7da0c8668d6922
Om Patil <patilom887@gmail.com>
2026-08-10 19:42:40 +0530

    dfs-traversal (pre-in-post)

--- Java-Backend-Learning/A3-DSA/Tree/TreeDemo.java
+++ Java-Backend-Learning/A3-DSA/Tree/TreeDemo.java
@@ -1,22 +1,5 @@
 package DSA.Tree;
 
-import com.sun.jdi.Value;
-
-import java.sql.Time;
-
-class Node {
-    int data;
-
-    Node left;
-    Node right;
-
-    Node(int data) {
-        this.data = data;
-        this.left = null;   // unintialzed members are bydefault null in java so no need to write this two lines
-        this.right = null;
-    }
-}
-
 //                            10
 //                          /    \
 //                        20      30
