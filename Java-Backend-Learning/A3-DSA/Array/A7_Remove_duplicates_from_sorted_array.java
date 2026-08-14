ceab6f660ad1c0680de3464af470a0d8cc27429c
Om Patil <patilom887@gmail.com>
2026-08-14 20:28:33 +0530

    remove duplicates from sorted array

---
+++ Java-Backend-Learning/A3-DSA/Array/A7_Remove_duplicates_from_sorted_array.java
@@ -0,0 +1,31 @@
+package DSA.Array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class A7_Remove_duplicates_from_sorted_array {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 1, 2, 2, 3, 4, 4};
+
+//        // Using HashSet which stores only unique values
+//        HashSet set = new HashSet();
+//        for (int i = 0; i < arr.length; i++) {
+//            set.add(arr[i]);
+//        }
+//        System.out.println(set);
+
+        int j = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }
+        }
+
+        for (int i = 0; i <= j; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
