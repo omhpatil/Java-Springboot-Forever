bfe7e87d86a0fe6c7134dc0f403989dc55ac4fee
Om Patil <patilom887@gmail.com>
2026-08-17 19:36:00 +0530

    one to one relationship working

--- Spring-Ashok-IT/21-OneToOne-Relationship/src/main/java/com/main/repository/PassportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.main.repository;
-
-import com.main.entity.Passport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PassportRepository extends JpaRepository<Passport, Integer> {
-}
--- Spring-Ashok-IT/21-OneToOne-Relationship/src/main/java/com/main/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.main.repository;
-
-import com.main.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-}
