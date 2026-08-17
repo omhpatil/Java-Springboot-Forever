34aa1d24dea944b556956b4d036ff88e71c09ea0
Om Patil <patilom887@gmail.com>
2026-08-17 21:14:29 +0530

    print pairs in array

---
+++ Java-Backend-Learning/A3-DSA/Array/PracticeQuestions/C02_Pairs_In_Array.java
@@ -0,0 +1,18 @@
+package DSA.Array.PracticeQuestions;
+
+public class C02_Pairs_In_Array {
+
+    public static void findPairs(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("[" + arr[i] + "," + arr[j] + "]");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10};
+        findPairs(arr);
+    }
+}
