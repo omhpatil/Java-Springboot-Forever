e4124740f63c87fc525a512498d0872c259f5a1a
Om Patil <patilom887@gmail.com>
2026-08-17 19:44:19 +0530

    one to one relationship working

---
+++ Spring-Ashok-IT/21-OneToOne-Relationship/src/main/java/com/main/entity/Person.java
@@ -0,0 +1,60 @@
+package com.main.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer personId;
+
+    private String personName;
+
+    private String personGender;
+
+    @OneToOne(mappedBy = "person", cascade = CascadeType.ALL)
+    private Passport passport;
+
+    public Integer getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Integer personId) {
+        this.personId = personId;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPersonGender() {
+        return personGender;
+    }
+
+    public void setPersonGender(String personGender) {
+        this.personGender = personGender;
+    }
+
+    public Passport getPassport() {
+        return passport;
+    }
+
+    public void setPassport(Passport passport) {
+        this.passport = passport;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "personId=" + personId +
+                ", personName='" + personName + '\'' +
+                ", personGender='" + personGender + '\'' +
+                ", passport=" + passport +
+                '}';
+    }
+}
