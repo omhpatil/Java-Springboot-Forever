bcccbd6c4ccb8f9195657454cf8aab7753888057
Om Patil <patilom887@gmail.com>
2026-08-22 09:54:07 +0530

    crud without using controller

---
+++ Spring-Ashok-IT/29-Spring-Data-Rest/src/main/java/com/main/entity/Student.java
@@ -0,0 +1,42 @@
+package com.main.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String gender;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ Spring-Ashok-IT/29-Spring-Data-Rest/src/main/java/com/main/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.main.repository;
+
+import com.main.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "students")
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
